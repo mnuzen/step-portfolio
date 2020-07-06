215730f3c3598afa8bb9c2860035fccf7a111b4f
Melba Nuzen <mnuzen@caltech.edu>
2020-07-06 11:52:15 -0700

    Testing graph functionality

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -64,3 +64,32 @@ function createListElement(text) {
   liElement.innerText = text;
   return liElement;
 }
+
+
+
+/** Chart Data */
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Lions', 10],
+          ['Tigers', 5],
+          ['Bears', 15]
+        ]);
+
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
