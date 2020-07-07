3d074401979c831e649cb6bda22f23273111166e
Melba Nuzen <mnuzen@caltech.edu>
2020-07-07 09:52:51 -0700

    Adding dependencies

--- portfolio/src/main/java/com/google/sps/servlets/WebDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/WebDataServlet.java
@@ -14,20 +14,15 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
 import java.io.IOException;
-import java.util.*; 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
 
 /** Servlet that processes website votes. */
 @WebServlet("/web-data")
