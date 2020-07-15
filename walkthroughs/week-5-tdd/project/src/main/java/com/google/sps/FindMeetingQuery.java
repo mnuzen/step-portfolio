c28c51448f1dc6b9c530f520ca5beb36cf8627e1
Melba Nuzen <mnuzen@caltech.edu>
2020-07-15 09:07:00 -0700

    Resolving comments from PR

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -26,14 +26,9 @@ import java.util.Set;
 import java.lang.*;
 
 public final class FindMeetingQuery {
-  /** MeetingRequest request has:
-      getAttendees() method which returns hashSet of mandatory attendees
-      getDuration() method which tells us how long our meeting has to be */
-
-  /** Collection<Event> events has events. Each event has:
-      getWhen() method which tells us time range of event
-      getAttendees() method which tells us who has to go to each event */
-
+  /** Determines all potential meeting times given a meeting request and a list of events for the day. Trims down list of events to only 
+    * hold events with mandatory attendees, then consolidates all busy time ranges into a list of time ranges. From that list of busy times, 
+    * pulls out potential solutions based on times that are not busy and still within the scope of the day. */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     Collection<String> mandatoryAttendees = request.getAttendees();
 
@@ -41,40 +36,54 @@ public final class FindMeetingQuery {
     MeetingRequest requestAllMandatory = new MeetingRequest (mandatoryAttendees, request.getDuration());
     ArrayList<TimeRange> solutions = new ArrayList<>();
 
-    // Requests with duration longer than a day, return no solutions. 
+    // Requests with duration longer than a day, return no solutions
     if (requestAllMandatory.getDuration() > TimeRange.WHOLE_DAY.duration()){
       return solutions;
     }
 
-    // If there are no attendees or meeting duration is 0, return whole day as solution. 
+    // If there are no attendees or meeting duration is 0, return whole day as solution 
     if (requestAllMandatory.getAttendees().isEmpty() || requestAllMandatory.getDuration() == 0){
       solutions.add(TimeRange.WHOLE_DAY);
       return solutions;
     }
 
+    // Iterate through all events, ensuring each event has at least one member from mandatoryAttendees    
+    ArrayList<TimeRange> relevantTimes = retrieveRelevantTimes(events, mandatoryAttendees);
+
+    // Condense overlapping ranges in events (assuming events are sorted)
+    ArrayList<TimeRange> consolidatedTimes = consolidateTimes(relevantTimes);
+
+    // Retrieve open intervals based on consolidated times
+    solutions = retrieveSolutions(solutions, consolidatedTimes, requestAllMandatory);
+    
+    return solutions;
+  }
+
+  /** Given a series of events and a list of mandatory attendees, trims down list of events until the only events remaining are ones
+    * that require mandatory attendees.  */
+  private ArrayList<TimeRange> retrieveRelevantTimes (Collection<Event> events, Collection<String> mandatoryAttendees) {
     ArrayList<TimeRange> relevantTimes = new ArrayList<>();
 
-    // Iterate through all events, ensuring each event has at least one member from mandatoryAttendees    
     Iterator<Event> iter = events.iterator();
     while (iter.hasNext()) {
       Event event = iter.next();
       Set<String> attendees = event.getAttendees();
-      boolean overlap = false;
 
       for (String attendee : attendees) {
         if (mandatoryAttendees.contains(attendee)){
-          overlap = true; // overlap is true if this event has a mandatory attendee
+          relevantTimes.add(event.getWhen()); // adding time if this event has a mandatory attendee
           break;
         }
       }
-
-      if (overlap) {
-        relevantTimes.add(event.getWhen());
-      }
     }
 
-    // Condense overlapping ranges in events (assuming events are sorted)
+    return relevantTimes;
+  }
+
+  /** Given a list of busy times, consolidates the time ranges that overlap to minimize time ranges. */
+  private ArrayList<TimeRange> consolidateTimes (ArrayList<TimeRange> relevantTimes) {
     ArrayList<TimeRange> consolidatedTimes = new ArrayList<>();
+
     for (TimeRange time : relevantTimes) {
       int solsEnd = consolidatedTimes.size()-1;
 
@@ -92,9 +101,16 @@ public final class FindMeetingQuery {
         consolidatedTimes.set(solsEnd, setTime); // replace latest busy time
       }
     }
-    
+
+    return consolidatedTimes;
+  }
+
+  /** Given a list of consolidated time ranges, retrieves solutions and potential free times based on the 24-hour interval that are not 
+    * overlapping with the busy times.*/
+  private ArrayList<TimeRange> retrieveSolutions (ArrayList<TimeRange> solutions, 
+                                                  ArrayList<TimeRange> consolidatedTimes, MeetingRequest requestAllMandatory) {
     if (!consolidatedTimes.isEmpty()) {
-      // edge case: front 
+      // edge case: if there's time between the start of the day and the start of the first event 
       int frontIndex = 0;
       int frontEndTime = consolidatedTimes.get(frontIndex).start();
       if (frontEndTime != TimeRange.START_OF_DAY) {
@@ -104,7 +120,7 @@ public final class FindMeetingQuery {
          }
       }
 
-      // core events 
+      // retrieve free times in between busy intervals 
       for (int i = 1; i < consolidatedTimes.size(); i++) {
         int prevStop = consolidatedTimes.get(i-1).end();
         int currStart = consolidatedTimes.get(i).start();
@@ -115,7 +131,7 @@ public final class FindMeetingQuery {
         prevStop = currStart;
       }
 
-      // edge case: back
+      // edge case: if there's time beteween the end of the last event and the end of the day
       int backIndex = consolidatedTimes.size()-1;
       int backStartTime = consolidatedTimes.get(backIndex).end();
       if (backStartTime != TimeRange.END_OF_DAY+1) {
@@ -126,9 +142,11 @@ public final class FindMeetingQuery {
       }
     }
     else {
+      // if there are no consolidated events, the whole day is free
       solutions.add(TimeRange.WHOLE_DAY);
     }
 
     return solutions;
   }
+
 }
