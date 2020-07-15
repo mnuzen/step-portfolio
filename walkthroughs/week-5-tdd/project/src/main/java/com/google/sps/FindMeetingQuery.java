7d4928f79d17f318b118eb12a6c73e9a85ee1cda
Melba Nuzen <mnuzen@caltech.edu>
2020-07-15 00:20:59 +0000

    Testing

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,72 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 
 public final class FindMeetingQuery {
+  /** MeetingRequest request has:
+      getAttendees() method which returns hashSet of mandatory attendees
+      getDuration() method which tells us how long our meeting has to be */
+
+  /** Collection<Event> events has events. Each event has:
+      getWhen() method which tells us time range of event
+      getAttendees() method which tells us who has to go to each event */
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> mandatoryAttendees = request.getAttendees();
+    ArrayList<TimeRange> solutions = new ArrayList<>();
+    System.out.println("testtttmelba");
+
+
+    //Set<Event> eventsSet = new HashSet<Event>(events);
+    //eventsSet.addAll(events);
+
+    // Iterate through all events, ensuring each event has at least one member from mandatoryAttendees    
+    Iterator<Event> iter = events.iterator();
+    while (iter.hasNext()) {
+      Event event = iter.next();
+      Set<String> attendees = event.getAttendees();
+      boolean overlap = false;
+
+      for (String attendee : attendees) {
+        if (mandatoryAttendees.contains(attendee)){
+          overlap = true; // overlap is true if this event has a mandatory attendee
+          break;
+        }
+      }
+
+      if (!overlap) {
+        iter.remove(); // remove event if mandatory attendees are not needed
+      }   
+    }
+
+    // Then condense overlapping ranges in events 
+    List<TimeRange> allRanges = Collections.emptyList();
+    List<TimeRange> sortedRanges = Collections.emptyList();
+    System.out.println("testtttmelba2");
+
+    for (Event event : events) {
+      allRanges.add(event.getWhen());
+      System.out.println(event.getTitle());
+    }
+
+    // Collections.sort(allRanges);
+
+
+
+    // and then find the free times in between each time range and check if its big enough
+
+
+    //throw new UnsupportedOperationException("TODO: Implement this method.");  
+    return null;
+
   }
 }
